b133d764ab42af0f99aeea205d4d08e6f8ee20d9
Nikolay Povstyanko <povstyankonikolai@gmail.com>
2022-07-15 13:10:13 +0300

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "@/views/HomeView.vue";
+import HomeView from "@/views/HomeView";
+import ProductPage from "@/views/ProductPage";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/product",
+    name: "product",
+    component: ProductPage,
   },
   // {
   //   path: "/about",
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="home">Home</div>
-  <v-header />
 </template>
 
 <script>
-import vHeader from "@/components/v-header.vue";
 export default {
   name: "HomeView",
-  components: { vHeader },
 };
 </script>
