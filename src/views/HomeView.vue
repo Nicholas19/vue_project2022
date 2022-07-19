7920f5bd923319a1e927ce088eab2b32d35aad13
ThePen4iK <64229562+ThePen4iK@users.noreply.github.com>
2022-07-19 13:33:18 +0300

    add footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,14 +5,16 @@
   <div class="main">
     <v-products></v-products>
   </div>
+  <v-footer></v-footer>
 </template>
 
 <script>
 import VProducts from "@/components/v-products";
 import vHeader from "@/components/v-header.vue";
+import vFooter from "@/components/v-footer.vue";
 
 export default {
-  components: { VProducts, vHeader },
+  components: { VProducts, vHeader, vFooter },
 
   name: "HomeView",
 };
