6f448cd7d41d5378a06f1d2e0df7166937c5536e
Vitalii Hoida <44941430+VitaliiHoida@users.noreply.github.com>
2022-07-28 13:46:47 +0300

    cartView

---
+++ src/components/AppCustomcheck.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <input
+      type="checkbox"
+      :name="name"
+      :id="name"
+      class="custom-checkbox"
+      v-model="checked"
+    />
+    <label :for="name"></label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppCustomcheck",
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  data: () => ({
+    checked: false,
+  }),
+};
+</script>
+
+<style scoped>
+div {
+  display: flex;
+  align-items: center;
+}
+.custom-checkbox {
+  position: absolute;
+  z-index: -1;
+  opacity: 0;
+}
+
+.custom-checkbox + label {
+  display: inline-flex;
+  align-items: center;
+  user-select: none;
+}
+
+.custom-checkbox + label::before {
+  content: "";
+  display: inline-block;
+  width: 27px;
+  height: 27px;
+  flex-shrink: 0;
+  flex-grow: 0;
+  border: 3px solid #898989;
+  border-radius: 6px;
+  margin-right: 40px;
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: 50% 50%;
+}
+
+.custom-checkbox:checked + label::before {
+  border-color: #ff7020;
+  background-color: #ff7020;
+  background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3cpath fill='%23fff' d='M6.564.75l-3.59 3.612-1.538-1.55L0 4.26 2.974 7.25 8 2.193z'/%3e%3c/svg%3e");
+}
+
+/* стили при наведении курсора на checkbox */
+.custom-checkbox:not(:disabled):not(:checked) + label:hover::before {
+  border-color: #ff7020;
+}
+
+/* стили для активного состояния чекбокса (при нажатии на него) */
+.custom-checkbox:not(:disabled):active + label::before {
+  background-color: #fff8f3;
+  border-color: #ff7020;
+}
+
+/* стили для чекбокса, находящегося в фокусе */
+.custom-checkbox:focus + label::before {
+  box-shadow: 0 0 0 0.2rem #fff8f3;
+}
+
+/* стили для чекбокса, находящегося в состоянии disabled */
+.custom-checkbox:disabled + label::before {
+  background-color: #e9ecef;
+}
+</style>
--- src/views/CartPage.vue
+++ src/views/CartPage.vue
@@ -1,14 +1,204 @@
 <template>
-  <section class="cart"></section>
+  <section class="cart">
+    <div class="container">
+      <div class="cart_wrapper">
+        <div class="cart_head">
+          <div class="cart_head-left">
+            <app-customcheck name="all"></app-customcheck>
+            <span class="sel">Select all</span>
+          </div>
+          <div class="cart_head-right">
+            <app-button name="UPDATE CART" variant="orange"></app-button>
+            <span class="divider"></span>
+            <app-button name="REMOVE" variant="red"></app-button>
+          </div>
+        </div>
+        <div class="cart-items">
+          <app-cartitem
+            v-for="item in items"
+            :key="item.sku"
+            :sku="item.sku"
+            :title="item.name"
+            :price="item.price"
+            :count="item.count"
+            :checked="false"
+          ></app-cartitem>
+        </div>
+      </div>
+      <app-asidecart>
+        <template v-slot:header> Shopping summary</template>
+        <template v-slot:main_content>
+          <div class="total">
+            <div class="total_title">Total</div>
+            <div class="total_sum">$ {{ totalSum }}</div>
+          </div>
+        </template>
+        <template v-slot:button>
+          <app-button
+            name="CHECKOUT"
+            variant="colored"
+            class="fw_btn"
+          ></app-button>
+        </template>
+        <template v-slot:footer>
+          <div class="footer_btn">
+            <router-link to="/" class="aside_link"
+              >Back to Shopping
+            </router-link>
+          </div>
+        </template>
+      </app-asidecart>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {};
+import AppButton from "@/components/AppButton.vue";
+import AppAsidecart from "@/components/AppAsidecart.vue";
+import AppCartitem from "@/components/AppCartitem.vue";
+import AppCustomcheck from "@/components/AppCustomcheck.vue";
+
+export default {
+  data: () => ({
+    items: [
+      {
+        name: "tv",
+        sku: "tv1",
+        count: 1,
+        price: 500,
+      },
+      { name: "phone", sku: "phone1", count: 1, price: 250 },
+      { name: "laptop", sku: "laptop1", count: 1, price: 400 },
+    ],
+  }),
+  computed: {
+    totalSum() {
+      let sum = 0;
+      this.items.forEach((item) => {
+        sum += item.count * item.price;
+      });
+      return sum;
+    },
+  },
+  components: {
+    AppButton,
+    AppAsidecart,
+    AppCartitem,
+    AppCustomcheck,
+  },
+};
 </script>
 
 <style scoped>
 .cart {
-  padding-top: 80px;
-  padding-bottom: 120px;
+  padding: 80px 0 120px;
+}
+
+.cart .container {
+  max-width: 1650px;
+  width: 100%;
+  padding: 0 15px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+}
+
+.cart_wrapper {
+  width: 1061px;
+}
+
+.cart_head {
+  height: 88px;
+  width: 100%;
+  background: #ffffff;
+  border: 1px solid #d5d5d5;
+  border-radius: 12px;
+  display: flex;
+  align-items: center;
+  padding: 30px 25px;
+  margin: 0 0 30px 0;
+  justify-content: space-between;
+}
+
+.cart_head-left,
+.cart_head-right {
+  display: flex;
+  align-items: center;
+}
+
+.cart_head-right {
+  margin-right: 30px;
+}
+
+.cart_head-right .divider {
+  height: 27px;
+  width: 2px;
+  background-color: #ededed;
+  border-radius: 8px;
+  margin: 0 27px;
+}
+
+.cart_head .sel {
+  font-family: "Lato";
+  font-style: normal;
+  font-weight: 800;
+  font-size: 16px;
+  line-height: 19px;
+  color: #000000;
+}
+
+.footer_btn {
+  text-align: center;
+  margin-top: 30px;
+}
+
+.fw_btn {
+  width: 100%;
+  border-radius: 12px;
+  font-size: 20px;
+  line-height: 24px;
+  padding: 20px;
+  margin-top: 30px;
+}
+
+.total {
+  display: flex;
+  justify-content: space-between;
+  font-family: "Poppins";
+  font-style: normal;
+  align-items: center;
+}
+
+.total_title {
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 27px;
+  /* identical to box height */
+  color: #000000;
+}
+
+.total_sum {
+  font-weight: 600;
+  font-size: 28px;
+  line-height: 42px;
+  /* identical to box height */
+  text-align: right;
+
+  color: #ff7020;
+}
+
+.aside_link {
+  font-family: "Lato";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 19px;
+
+  color: #ff7020;
+}
+
+.aside_link:hover {
+  color: #7b7b7b;
 }
 </style>
